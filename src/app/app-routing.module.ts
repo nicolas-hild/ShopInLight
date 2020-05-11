65e07e913ebe2f8200fe69418cddb9b8b67124d9
nicolas hild <hild_nicolas@yahoo.fr>
2020-05-11 13:46:07 +0200

    navbar working and all navigations routes working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { OrderSuccessComponent } from './order-success/order-success.component';
 import { LoginComponent } from './login/login.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
 
 
 const routes: Routes = [
@@ -15,6 +16,7 @@ const routes: Routes = [
   { path: 'products', component: ProductsComponent},
   { path: 'shopping-cart', component: ShoppingCartComponent},
   { path: 'check-out', component: CheckOutComponent},
+  { path: 'my/orders', component: MyOrdersComponent},
   { path: 'order-success', component: OrderSuccessComponent},
   { path: 'login', component: LoginComponent },
   { path: 'admin/products', component: AdminProductsComponent},
