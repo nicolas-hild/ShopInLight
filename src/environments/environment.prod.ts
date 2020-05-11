672c2177ee8fca27693b73a75bc379ec53667bf8
nicolas hild <hild_nicolas@yahoo.fr>
2020-05-11 11:58:46 +0200

    add all components in app folder and add all routes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCFa8RZCpynJmQobqseHZXEn3XLcRGiWkg",
+    authDomain: "shop-in-light.firebaseapp.com",
+    databaseURL: "https://shop-in-light.firebaseio.com",
+    projectId: "shop-in-light",
+    storageBucket: "shop-in-light.appspot.com",
+    messagingSenderId: "498035141911",
+    appId: "1:498035141911:web:7c81558e009694daed1b52"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCFa8RZCpynJmQobqseHZXEn3XLcRGiWkg",
+    authDomain: "shop-in-light.firebaseapp.com",
+    databaseURL: "https://shop-in-light.firebaseio.com",
+    projectId: "shop-in-light",
+    storageBucket: "shop-in-light.appspot.com",
+    messagingSenderId: "498035141911",
+    appId: "1:498035141911:web:7c81558e009694daed1b52"
+  }
 };
 
 /*
